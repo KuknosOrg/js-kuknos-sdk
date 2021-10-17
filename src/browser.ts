3e6b8b0c14c50fee2918fa78baddf32b6f7a74a4
parvanesh93 <shojaee.pc1393@gmail.com>
2021-10-17 14:09:26 +0330

    initial

--- src/browser.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable:no-var-requires */
-
-module.exports = require("./index");
-module.exports.axios = require("axios");
-module.exports.StellarBase = require("js-kuknos-base");
-
-export { };
